3280778c9b39974d293602d00e610f00f976210e
Bibibouba <VTSeniors.France@gmail.com>
2025-04-20 00:28:49 +0200

    Update TripSummaryStep.tsx

--- src/components/quote/form/TripSummaryStep.tsx
+++ src/components/quote/form/TripSummaryStep.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { TripHeaderCard } from './steps/TripHeaderCard';
 import { MinDistanceAlert } from './summary/MinDistanceAlert';
@@ -37,13 +36,25 @@ const TripSummaryStep: React.FC<TripSummaryStepProps> = (props) => {
   const selectedVehicleInfo = props.vehicles.find(v => v.id === props.selectedVehicle);
   const hasMinDistanceWarning = props.quoteDetails?.hasMinDistanceWarning;
   const minDistance = props.quoteDetails?.minDistance || 0;
-  
+
+  // 1) Calculez la distance totale (aller + retour si demandé)
+  const totalDistance =
+    props.estimatedDistance +
+    (props.hasReturnTrip ? props.returnDistance : 0);
+
+  // 2) Calculez la durée totale en minutes (aller + retour + attente)
+  const totalDurationMinutes =
+    props.estimatedDuration +
+    (props.hasReturnTrip ? props.returnDuration : 0) +
+    (props.hasWaitingTime ? props.waitingTimeMinutes : 0);
+
+  // 3) Recalcul de l'heure de fin en fonction de la durée totale
   const tripEndTime = (() => {
-    const [hours, minutes] = props.time.split(':').map(Number);
-    const arrivalTime = new Date();
-    arrivalTime.setHours(hours);
-    arrivalTime.setMinutes(minutes + props.estimatedDuration);
-    return arrivalTime;
+    const [h, m] = props.time.split(':').map(Number);
+    const t = new Date();
+    t.setHours(h);
+    t.setMinutes(m + totalDurationMinutes);
+    return t;
   })();
 
   return (
@@ -61,31 +72,36 @@ const TripSummaryStep: React.FC<TripSummaryStepProps> = (props) => {
         hasWaitingTime={props.hasWaitingTime}
         waitingTimeMinutes={props.waitingTimeMinutes}
       />
-      
+
       {hasMinDistanceWarning && (
-        <MinDistanceAlert 
+        <MinDistanceAlert
           estimatedDistance={props.estimatedDistance}
           minDistance={minDistance}
         />
       )}
-      
+
       <TripTimeInfo
         startTime={props.time}
-        endTime={tripEndTime.getHours().toString().padStart(2, '0') + ':' + 
-                tripEndTime.getMinutes().toString().padStart(2, '0')}
+        endTime={
+          tripEndTime.getHours().toString().padStart(2, '0') + ':' +
+          tripEndTime.getMinutes().toString().padStart(2, '0')
+        }
         nightRateInfo={props.quoteDetails?.nightRateInfo}
         returnNightRateInfo={props.quoteDetails?.returnNightRateInfo}
         sundayRateInfo={props.quoteDetails?.sundayRateInfo}
         hasReturnTrip={props.hasReturnTrip}
       />
-      
+
       <TripDetailsGrid
         {...props}
+        // on écrase les valeurs renvoyées initialement pour afficher les totaux
+        estimatedDistance={totalDistance}
+        estimatedDuration={totalDurationMinutes}
         isNightRate={!!props.quoteDetails?.isNightRate}
         isSunday={!!props.quoteDetails?.isSunday}
         nightHours={props.quoteDetails?.nightHours || 0}
       />
-      
+
       <FormNavigation
         handlePreviousStep={props.handlePreviousStep}
         handleNextStep={props.handleNextStep}
